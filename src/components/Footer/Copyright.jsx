dfcdbef63ee25fd3575408ed01f9bce3b702b708
xkl <joselucas20048jl@gmail.com>
2024-06-19 11:01:43 -0300

    logo and CR

--- src/components/Footer/Copyright.jsx
+++ src/components/Footer/Copyright.jsx
@@ -5,6 +5,7 @@ export const Copyright = () => {
       <div className="w-[700px] mt-1 p-[5px] ml-[px] h-[25px]">
         <span className=" text-[10px]">Copyright © 2019. Todos os direitos reservados. Todas as marcas e suas imagens são de propriedade de seus respectivos donos.  <br /> É vedada a reprodução, total ou parcial, de qualquer conteúdo sem expressa autorização.</span>
       </div>
+      <h1 className='font-bold mt-4 hover:text-violet-600 hover:translate-x-3 duration-500'>_Made by: xKL </h1>
       <img className='p-6 mr-10' src={logo} alt="" />
     </div>
   )
